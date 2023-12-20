8ad4837013ba5b292095f52c3482f14322dd6e3d
luisangelalvarez <149668358+luisangelalvarez@users.noreply.github.com>
2023-12-20 20:21:31 +0100

    Update constructor.js

--- js/constructor.js
+++ js/constructor.js
@@ -1,4 +1,3 @@
-// Constructor del catálogo
 document.addEventListener("DOMContentLoaded", function () {
   const carouselContainer = document.getElementById("carousel-products");
   fetch("./json/catalogo.json")
@@ -8,28 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
         const article = document.createElement("article");
         article.className = "product";
 
+        const a = document.createElement('a');
+        a.href = `./html/${movil.clave}.html`;
+
         const figure = document.createElement("figure");
+
         const img = document.createElement("img");
         img.className = "image-product";
         img.src = movil.ruta_imagen;
         img.alt = movil.nombre;
 
-        const divInfo = document.createElement("div");
-        divInfo.className = "info-product";
-        const p = document.createElement("p");
-        p.textContent = movil.nombre;
-
-        const a = document.createElement('a');
-        a.href = `./html/${movil.clave}.html`;
         const button = document.createElement('button');
         button.textContent = 'Ver';
+        button.className = 'main-button'; // Añade una clase al botón
 
-        a.appendChild(button);
-        divInfo.appendChild(p);
-        divInfo.appendChild(a);
         figure.appendChild(img);
-        article.appendChild(figure);
-        article.appendChild(divInfo);
+        figure.appendChild(button);
+
+        a.appendChild(figure);
+        article.appendChild(a);
         carouselContainer.appendChild(article);
       });
     })
@@ -39,19 +35,54 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
 function muestraBusqueda() {
-  let a = document.getElementById("frameBuscar").style.display;
-  if (a === "block") {
-    document.getElementById("frameBuscar").style.display = "none";
-  } else {
-    document.getElementById("frameBuscar").style.display = "block";
-    document.getElementById("inpBusq").focus();
-  }
-}
-const input = document.getElementById("inpBusq");
-input.addEventListener("keyup", function (event) {
-  if (event.key === "Enter") {
-    console.log("Tecla Enter presionada");
-    alert(1);
-  }
+   let a = document.getElementById("frameBuscar").style.display;
+   if (a === "block") {
+     document.getElementById("frameBuscar").style.display = "none";
+   } else {
+     document.getElementById("frameBuscar").style.display = "block";
+     document.getElementById("inpBusq").focus();
+   }
+ };
+
+
+ document.addEventListener("DOMContentLoaded", function () {
+  
+  const searchInput = document.getElementById("inpBusq");
+  searchInput.addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      const searchText = searchInput.value.toLowerCase(); // Obtener el texto ingresado y convertirlo a minúsculas para la comparación
+      fetch("./json/catalogo.json")
+        .then((response) => response.json())
+        .then((data) => {
+          const filteredMobiles = data.moviles.filter(
+            (movil) =>
+              movil.nombre.toLowerCase().includes(searchText) // Filtrar por nombre que contiene el texto ingresado
+          );
+          if (filteredMobiles.length > 0) {
+            document.getElementById("main-offer").style.display = "none";
+            document.getElementById("carousel-products").style.display = "none";
+            const productList = document.createElement("ul");
+            filteredMobiles.forEach((movil) => {
+              const listItem = document.createElement("li");
+              const link = document.createElement("a");
+              link.href = `./html/${movil.clave}.html`; // Redirigir a productx.html
+              link.innerHTML = `<img src='${movil.ruta_imagen}' alt='movil.nombre'>Nombre: ${movil.nombre} Precio: ${movil.precio} Color: ${movil.color} Descripción: ${movil.descripcion}`;
+              listItem.appendChild(link);
+              productList.appendChild(listItem);
+              document.getElementById("frameBuscar").style.display = "none";
+            });
+            const mainProduct = document.querySelector(".productoBuscado");
+            mainProduct.innerHTML = ""; // Limpiar el contenido existente en .main-product
+            mainProduct.appendChild(productList); // Agregar la lista de resultados filtrados
+          } else {
+            alert("No se encontraron resultados");
+          }
+        })
+        .catch((error) =>
+          alert("Error al obtener el archivo JSON: ", error)
+        );
+    }
+  });
 });
