e6bb164e8add78ef4be87d3bbd4c2cdaf9c820b6
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2023-12-21 12:21:08 +0100

    actualizacion de script cart y technical service

--- js/technicalservice.js
+++ js/technicalservice.js
@@ -1,3 +1,14 @@
+//Validaciones del login/registro
+
+// Expresiones regulares para validación
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+
+// Función para validar campos
+function validateFields(email, password) {
+  return emailRegex.test(email) && passwordRegex.test(password);
+}
+
 function enviarMensaje() {
   // Puedes agregar aquí la lógica para enviar el mensaje (por ejemplo, mediante una solicitud AJAX)
   alert(
